d30fca3da14dea1d7cfda184b0d072cb6cc842e2
v.rublova <v.rublova@student.csn.khai.edu>
2020-11-12 20:45:31 +0200

    Initial commit

---
+++ Appliances/src/Appliances/Terrarium.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Appliances;
+
+/**
+ *
+ * @author v.rublova
+ */
+public class Terrarium implements Thing {
+    public void print() {
+        System.out.println("Fishy!");
+    }  
+}
---
+++ src/smarthome/SmartHome.java
@@ -0,0 +1,18 @@
+package smarthome;
+
+import Appliances.Terrarium;
+
+/**
+ *
+ * @author v.rublova
+ */
+public class SmartHome {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       Terrarium terra = new Terrarium();
+    }
+    
+}
